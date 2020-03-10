6ea047a09819c15096ee89e33721797a72a1df8f
antoine.viala <antoine.viala@epitech.eu>
2020-03-10 10:03:25 +0100

    mv mvprintw to mvaddch

--- map.c
+++ map.c
@@ -11,17 +11,17 @@ void display_map(void)
     int x = 10;
     int y = 20;
 
-    mvprintw(1, 33, "+");
-    mvprintw(1, 33 + x * 2 + 2, "+");
-    mvprintw(2 + y, 33, "+");
-    mvprintw(2 + y, 33 + x * 2 + 2, "+");
+    mvaddch(1, 33, '+');
+    mvaddch(1, 33 + x * 2 + 2, '+');
+    mvaddch(2 + y, 33, '+');
+    mvaddch(2 + y, 33 + x * 2 + 2, '+');
     for (int i = 1; i <= x * 2 + 1; i++) {
-        mvprintw(1, 33 + i, "-");
-        mvprintw(2 + y, 33 + i, "-");
+        mvaddch(1, 33 + i, '-');
+        mvaddch(2 + y, 33 + i, '-');
     }
     for (int i = 1; i < y + 1; i++) {
-        mvprintw(1 + i, 33, "|");
-        mvprintw(1 + i, 33 + x * 2 + 2, "|");
+        mvaddch(1 + i, 33, '|');
+        mvaddch(1 + i, 33 + x * 2 + 2, '|');
     }
 }
 
@@ -30,16 +30,16 @@ void display_scoreboard(void)
     int x = 11;
     int y = 8;
 
-    mvprintw(8, 1, "/");
-    mvprintw(8, 1 + x * 2 + 2, "\\");
-    mvprintw(9 + y, 1, "\\");
-    mvprintw(9 + y, 1 + x * 2 + 2, "/");
+    mvaddch(8, 1, '/');
+    mvaddch(8, 1 + x * 2 + 2, '\\');
+    mvaddch(9 + y, 1, '\\');
+    mvaddch(9 + y, 1 + x * 2 + 2, '/');
     for (int i = 1; i <= x * 2 + 1; i++) {
-        mvprintw(8, 1 + i, "-");
-        mvprintw(9 + y, 1 + i, "-");
+        mvaddch(8, 1 + i, '-');
+        mvaddch(9 + y, 1 + i, '-');
     }
     for (int i = 1; i < y + 1; i++) {
-        mvprintw(8 + i, 1, "|");
-        mvprintw(8 + i, 1 + x * 2 + 2, "|");
+        mvaddch(8 + i, 1, '|');
+        mvaddch(8 + i, 1 + x * 2 + 2, '|');
     }
 }
