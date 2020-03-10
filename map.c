044d94e4a2e28be81744bc1be0f52d0171f04230
antoine.viala <antoine.viala@epitech.eu>
2020-03-10 19:05:49 +0100

    diplsay_tetris fini

--- map.c
+++ map.c
@@ -48,12 +48,40 @@ void display_scoreboard(void)
         mvaddch(8 + i, 1 + x * 2 + 2, '|');
     }
 }
+static void diplsay_tre_last(void)
+{
+    attrset(COLOR_PAIR(3));
+    mvaddstr(0, 12, "* * *");
+    for (int i = 0; ++i < 5;)
+        mvaddch(i, 14, '*');
+    attrset(COLOR_PAIR(4));
+    mvaddstr(0, 18, "* * *");
+    mvaddstr(1, 18, "*   *");
+    mvaddstr(2, 18, "* *");
+    mvaddstr(3, 18, "*   *");
+    mvaddstr(4, 18, "*   *");
+    attrset(COLOR_PAIR(5));
+    mvaddch(0, 24, '*');
+    for (int i = 1; ++i < 5;)
+        mvaddch(i, 24, '*');
+    attrset(COLOR_PAIR(6));
+    for (int i = 0; i < 5; i += 2)
+        mvaddstr(i, 26, "* * *");
+    mvaddch(1, 26, '*');
+    mvaddch(3, 30, '*');
+}
 
 void diplsay_tetris(void)
 {
-    mvaddstr(0, 0, "*************");
-    mvaddstr(0, 0, " * *   * * * *");
-    mvaddstr(0, 0, " * **  * * ** ****");
-    mvaddstr(0, 0, " * *   * * **  *");
-    mvaddstr(0, 0, " * *** * * *****");
+    attrset(COLOR_PAIR(1));
+    mvaddstr(0, 0, "* * *");
+    for (int i = 0; ++i < 5;)
+        mvaddch(i, 2, '*');
+    attrset(COLOR_PAIR(2));
+    mvaddstr(0, 6, "* * *");
+    mvaddch(1, 6, '*');
+    mvaddstr(2, 6, "* *");
+    mvaddch(3, 6, '*');
+    mvaddstr(4, 6, "* * *");
+    diplsay_tre_last();
 }
