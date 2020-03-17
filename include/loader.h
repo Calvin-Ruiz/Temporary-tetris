487ceb939ccc9cc4345fa66fd1727741ca7f68d5
Calvin Ruiz <calvin.ruiz@epitech.eu>
2020-03-17 14:12:15 +0100

    finish loader core structure

--- include/loader.h
+++ include/loader.h
@@ -8,6 +8,8 @@
 #ifndef LOADER_H_
 #define LOADER_H_
 
+#include <dict.h>
+
 typedef struct loader {
     char *name;
     piece_t *piece;
@@ -15,7 +17,9 @@ typedef struct loader {
 } loader_t;
 
 char *create_rotated_piece(char *piece_array, vec_t *size);
-void append_piece_from_file(const char *filename, loader_t **loader);
-piece_t *load_piece_array(char is_debug);
+void append_piece_from_file(const char *filename, dict_t **loader);
+piece_t **load_piece_array(char is_debug);
+piece_t **build_piece_array(loader_t * loader);
+void debug_display(loader_t *loader);
 
 #endif /* LOADER_H_ */
--- loader.c
+++ loader.c
@@ -7,3 +7,21 @@
 
 #include <piece.h>
 #include <loader.h>
+
+piece_t **load_piece_array(char is_debug)
+{
+    dict_t *loader = NULL;
+    DIR *dir = opendir("tetriminos");
+    struct dirent *my_dirent = readdir(dir);
+    piece_t *pieces;
+
+    while (my_dirent) {
+        append_piece_from_file(my_dirent->d_name, &loader);
+        my_dirent = readdir(dir);
+    }
+    if (is_debug)
+        debug_display((loader_t *) loader);
+    pieces = build_piece_array((loader_t *) loader);
+    destroy_dict(loader);
+    return (pieces);
+}
