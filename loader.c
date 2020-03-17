22c7b71a144c60af16cb0f25c84f63d7656407c7
antoine.viala <antoine.viala@epitech.eu>
2020-03-17 19:43:24 +0100

    perser_tetrimino pas encor fini

--- loader.c
+++ loader.c
@@ -9,6 +9,35 @@
 #include <loader.h>
 #include <dirent.h>
 
+piece_t *parser_tetrimino(int fd, piece_t *piece)
+{
+    char *line = get_next_line(fd);
+    char **tab;
+    if (line == NULL)
+        return (NULL);
+    piece->size.x;
+    if () {
+        piece->size.y;
+        piece->color;
+    } else
+        return (NULL);
+    return (piece);
+}
+
+void append_piece_from_file(const char *filename, dict_t **loader)
+{
+    piece_t *piece = malloc(sizeof(piece_t));
+    int fd = open(filename, O_RDONLY);
+
+    if (piece == NULL || fd == -1)
+        return;
+    piece = parser_tetrimino(fd, piece);
+    if (piece == NULL)
+        return;
+    close(fd);
+    append_to_dict(loader, filename, piece);
+}
+
 piece_t **load_piece_array(char is_debug, uchar_t *nb_valid_pieces)
 {
     dict_t *loader = NULL;
