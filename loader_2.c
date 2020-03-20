8ba313626322fc38db73b0c7f7fbc79dd33e42b2
Calvin Ruiz <calvin.ruiz@epitech.eu>
2020-03-20 18:46:16 +0100

    Implement rotations

--- loader_2.c
+++ loader_2.c
@@ -29,5 +29,14 @@ piece_t **build_piece_array(loader_t *loader, uchar_t *nb_valid_pieces)
 
 uchar_t *create_rotated_piece(uchar_t *piece_array, vec_t *size)
 {
-    return (piece_array);
+    ushort_t y = size->y;
+    uchar_t *display = malloc(size->x * size->y);
+
+    if (display == NULL)
+        return (piece_array);
+    while (y-- > 0) {
+        for (ushort_t x = -1; ++x < size->x;)
+            display[y + x * size->y] = *(piece_array++);
+    }
+    return (display);
 }
